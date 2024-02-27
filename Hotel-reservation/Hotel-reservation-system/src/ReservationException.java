44d75eb0ffd374bf09302c6fa942792ef6ff5b95
YpCIIIaK <119199773+YpCIIIaK@users.noreply.github.com>
2024-02-27 11:56:04 +0600

    Delete Hotel-reservation/Hotel-reservation-system directory

--- Hotel-reservation/Hotel-reservation-system/src/ReservationException.java
+++
@@ -1,5 +0,0 @@
-public class ReservationException extends Exception {
-    public ReservationException(String message) {
-        super(message);
-    }
-}
--- Hotel-reservation/Hotel-reservation-system/src/RoomNotAvailableException.java
+++
@@ -1,5 +0,0 @@
-public class RoomNotAvailableException extends ReservationException {
-    public RoomNotAvailableException(String message) {
-        super(message);
-    }
-}
